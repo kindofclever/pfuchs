b0ae5ab8ff0d096c6287f24513be2e1a45cd7d66
Sandra <sandra.gufler@gmx.ch>
2023-09-05 16:43:30 +0200

    Added home component and set up the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import Theme from './Theme';
 import LandingPage from './containers/Landing/LandingPage';
+import { Home } from './containers/Home/Home';
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={Theme}>
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<LandingPage />}
+          />
+          <Route
+            path='/home'
+            element={<Home />}
+          />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
--- src/containers/Landing/LandingPage.tsx
+++ src/containers/Landing/LandingPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { motion } from 'framer-motion';
-import LandingBanner from './LandingBanner';
+import { useNavigate } from 'react-router-dom';
+
+import LandingBanner from './components/LandingBanner';
 import CustomButton from '../../components/CustomButton';
 import { buttonVariants } from '../../variants/variants';
 
@@ -9,6 +11,12 @@ const imageURL =
   'https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=4000&q=80';
 
 const LandingPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/home');
+  };
+
   return (
     <Box
       sx={{
@@ -33,7 +41,12 @@ const LandingPage: React.FC = () => {
         animate='visible'
         variants={buttonVariants}
       >
-        <CustomButton btnType={'action'}>Come In</CustomButton>
+        <CustomButton
+          btnType={'action'}
+          onClick={handleClick}
+        >
+          Come In
+        </CustomButton>
       </motion.div>
     </Box>
   );
